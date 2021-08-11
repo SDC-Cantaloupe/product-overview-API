cbc6c63f25b000769b21fb719e1b9c45ae0dc97e
Jennifer Yang <jennifer.jinyoung.yang@gmail.com>
2021-08-10 23:41:31 -0400

    Deleted unnecessary commented lines

--- routes.js
+++ routes.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
 const db = require('./db');
 
-// router.get('/products', (req, res) => {
-// // addtional parameters - page (INT, default 1) / count (INT, default 5)  - might not be needed
-//   res.status(200).send('get request to /products');
-// });
 
 router.get('/products/:product_id', (req, res) => {
   db.getProductInfo(req.params['product_id'], (err, data) => {
@@ -36,14 +32,6 @@ router.get('/products/:product_id/related', (req, res) => {
   });
 });
 
-// router.get('/cart', (req, res) => {
-//   res.status(200).send('get request for /cart');
-// });
-
-// router.post('/cart', (req, res) => {
-//   // body parameter sku_id INT ID for the product being added to the cart
-//   res.status(201).send('post request for /cart');
-// });
 
 module.exports = router;
 
--- tests/integration.test.js
+++ tests/integration.test.js
@@ -13,15 +13,6 @@ afterAll(() => {
 
 describe('end-to-end test', () => {
 
-  // it('test get request to the endpoint /products', (done) => {
-  //   request.get('/products')
-  //     .expect(200)
-  //     .expect((res) => {
-  //       expect(res.text).toBe('get request to /products');
-  //     })
-  //     .end(done);
-  // });
-
   it('test get request to the endpoint /products/:product_id', (done) => {
     request.get('/products/1')
       .expect(200)
@@ -49,24 +40,6 @@ describe('end-to-end test', () => {
       })
       .end(done);
   });
-
-  // it('test get request to the endpoint /cart', (done) => {
-  //   request.get('/cart')
-  //     .expect(200)
-  //     .expect((res) => {
-  //       expect(res.text).toBe('get request for /cart');
-  //     })
-  //     .end(done);
-  // });
-
-  // it('test post request to the endpoint /cart', (done) => {
-  //   request.post('/cart')
-  //     .expect(201)
-  //     .expect((res) => {
-  //       expect(res.text).toBe('post request for /cart');
-  //     })
-  //     .end(done);
-  // });
 
 });
 
